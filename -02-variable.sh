b1803d306188a8b572437f44014939dce30ce72f
priya <priyadaws86@gmail.com>
2026-04-24 19:04:26 +0530

    variables

---
+++ -02-variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:: Hello Dev, how are you doing?"
+echo "$PERSON2:: I am doing good, how about you?"
+echo "$PERSON1:: I am doing good as well, thanks for asking."
+echo "$PERSON2:: So, what are you working on these days?"
+echo "$PERSON1:: I am working on a shell script to practice variables and functions."
