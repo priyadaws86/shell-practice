7eec619d3826ab4a716edda6c6e4efed8a318ae5
priya <priyadaws86@gmail.com>
2026-04-27 12:58:00 +0530

    printing date

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -4,6 +4,8 @@ NUMBER=$1
 
 if [ $NUMBER -gt 10 ]; then
     echo "The number is greater than 10"
+elif [ $NUMBER -eq 10 ]; then
+    echo "The number is equal to 10"    
 else
     echo "The number is less than 10"
 fi
